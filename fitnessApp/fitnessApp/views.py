7ef167a8c0afe0558a5c05bfff61dd737b177774
sanjshroff <sanjshroff2@gmail.com>
2021-10-04 16:17:15 -0300

    db connected

--- fitnessApp/fitnessApp/views.py
+++ fitnessApp/fitnessApp/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
+import MySQLdb
+
 
 def index(request):
+    db = MySQLdb.connect("localhost", "root", "admin123", "fitnessdb")
+    cursor = db.cursor()
+    query = """select * from instructor;"""
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for r in results:
+        print(r)
+    cursor.close()
+    db.close()
     return HttpResponse("<h1>Welcome to Fitness World</h1>")
 
 def sessions(request):
